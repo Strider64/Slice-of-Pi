1a4526c8d1ccdf6e6da5c4016992a2beff3f5b8a
John Pepp <jrpepp@pepster.com>
2017-07-04 11:49:16 -0400

    Add files via upload

---
+++ Private/includes/footer.inc.php
@@ -0,0 +1,19 @@
+<footer class="container footer-style">
+    <p class="footer-name">&copy;<?php echo date("Y"); ?> <span>John R. Pepp</span></p>
+</footer>
+<script>
+    var yesCheck = document.getElementById("yesCheck");
+    var noCheck = document.getElementById("noCheck");
+
+    yesCheck.addEventListener('click', function () {
+        document.getElementById('imgBtn').style.display = "block";
+    });
+
+    noCheck.addEventListener('click', function () {
+        document.getElementById('imgBtn').style.display = "none";
+    });
+
+</script>
+</body>
+</html>
+
