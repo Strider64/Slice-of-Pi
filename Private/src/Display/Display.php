a2563b570b9f4702876cf2728399827eee5dbb46
John Pepp <jrpepp@pepster.com>
2017-07-25 08:01:56 -0400

    Add files via upload

--- Private/src/Display/Display.php
+++ Private/src/Display/Display.php
@@ -2,7 +2,7 @@
 
 namespace Library\Display;
 
-use  Library\Read\Read;
+use Library\Read\Read;
 
 class Display extends Read {
 
@@ -15,22 +15,20 @@ class Display extends Read {
     public function __construct() {
         
     }
+    public function setData(array $data) {
+        $this->data = $data;
+    }
 
     public function display() {
         foreach ($this->data as $this->row) {
-            if ($this->row->heading) {
-                echo "<h1>" . htmlspecialchars($this->row->heading) . "</h1>\n";
-                echo '<h2 class="subheading">Created on ' . htmlspecialchars($this->row->date_added) . " by " . htmlspecialchars($this->row->author) . "</h2>\n";
-            } 
-
-            if ($this->row->image_path) {
-                echo '<figure class="imageStyle">' . "\n";
-                echo '<img src="' . htmlspecialchars($this->row->image_path) . '" alt="' . htmlspecialchars($this->row->heading) . '">' . "\n";
-                echo '<figcaption>&nbsp;</figcaption>' . "\n";
-                echo "</figure>\n";
-            }
+            echo "<h1>" . htmlspecialchars($this->row->heading) . "</h1>\n";
+            echo '<h2 class="subheading">Created on ' . htmlspecialchars($this->row->date_added) . " by " . htmlspecialchars($this->row->author) . "</h2>\n";
+            echo '<figure class="imageStyle">' . "\n";
+            echo '<img src="' . htmlspecialchars($this->row->image_path) . '" alt="' . htmlspecialchars($this->row->heading) . '">' . "\n";
+            echo '<figcaption>&nbsp;</figcaption>' . "\n";
+            echo "</figure>\n";
             echo "<p>" . htmlspecialchars($this->row->content) . "</p>\n";
-            if (isset($_SESSION['user']) && ($_SESSION['user']->security_level === 'sysop' ||  $_SESSION['user']->id === $this->row->user_id)) {
+            if (isset($_SESSION['user']) && ($_SESSION['user']->security_level === 'sysop' || $_SESSION['user']->id === $this->row->user_id)) {
                 echo '<div id="system">' . "\n";
                 echo '<a id="edit" href="edit/' . urlencode($this->row->id) . '">Edit</a>' . "\n";
                 echo '<a id="delete" href="delete_page.php?id=' . urlencode($this->row->id) . '">Delete</a>' . "\n";
@@ -40,7 +38,7 @@ class Display extends Read {
         }
     }
 
-    public function read($page_name, $column_pos, $sort_by = "ASC", $user_id=65) {
+    public function read($page_name, $column_pos, $sort_by = "ASC", $user_id = 65) {
         $this->data = parent::Read($page_name, $column_pos, $sort_by);
     }
 
