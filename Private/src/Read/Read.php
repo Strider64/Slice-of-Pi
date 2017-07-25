756aed2202371824f138b4c9f48eae9c872b044f
John Pepp <jrpepp@pepster.com>
2017-07-25 08:02:29 -0400

    Add files via upload

--- Private/src/Read/Read.php
+++ Private/src/Read/Read.php
@@ -18,14 +18,24 @@ abstract class Read {
         $this->stmt = $pdo->prepare($this->query);
         $this->stmt->execute();
         $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
-        return $this->data;        
+        return $this->data;
     }
 
-    public function read($page_name, $column_pos, $sort_by = "ASC", $user_id = 65) {
+    public function dailyRead($blogDate) {
+        $db = DB::getInstance();
+        $pdo = $db->getConnection();
+        $this->query = 'SELECT id, user_id, author, page_name, column_pos, image_path, heading, content, DATE_FORMAT(date_added, "%W, %M %e, %Y") as date_added, date_added as myDate FROM cms WHERE DATE_FORMAT(date_added, "%Y-%m-%e")=:date_added ORDER BY author  ASC';
+        $this->stmt = $pdo->prepare($this->query); // Prepare the query:
+        $this->stmt->execute([':date_added' => $blogDate]); // Execute the query with the supplied user's parameter(s):
+        $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        return $this->data;
+    }
+
+    public function read($page_name, $column_pos, $sort_by = "ASC") {
         $db = DB::getInstance();
         $pdo = $db->getConnection();
         if ($sort_by === "DESC") {
-            $this->query = 'SELECT id, user_id, author, page_name, column_pos, image_path, heading, content, DATE_FORMAT(date_added, "%W, %M %e, %Y") as date_added, date_added as myDate FROM cms WHERE page_name=:page_name AND column_pos=:column_pos ORDER BY myDate DESC, user_id ASC';
+            $this->query = 'SELECT id, user_id, author, page_name, column_pos, image_path, heading, content, DATE_FORMAT(date_added, "%W, %M %e, %Y") as date_added, date_added as myDate FROM cms WHERE page_name=:page_name AND column_pos=:column_pos ORDER BY myDate DESC, author  DESC';
         } else {
             $this->query = 'SELECT id, user_id, author, page_name, column_pos, image_path, heading, content, DATE_FORMAT(date_added, "%W, %M %e, %Y") as date_added, date_added as myDate FROM cms WHERE page_name=:page_name AND column_pos=:column_pos ORDER BY user_id ASC, myDate DESC';
         }
