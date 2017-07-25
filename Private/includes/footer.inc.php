c235d9697f3fa0cc9bad65e03b842b576b9cea2f
John Pepp <jrpepp@pepster.com>
2017-07-25 07:59:27 -0400

    Add files via upload

--- Private/includes/footer.inc.php
+++ Private/includes/footer.inc.php
@@ -2,6 +2,34 @@
     <p class="footer-name">&copy;<?php echo date("Y"); ?> <span>John R. Pepp</span></p>
 </footer>
 <script>
+    var slides = document.querySelectorAll('#slides .slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 3000);
+
+    function nextSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+</script>
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-102592896-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+<script>
     var yesCheck = document.getElementById("yesCheck");
     var noCheck = document.getElementById("noCheck");
 
--- Private/includes/header.inc.php
+++ Private/includes/header.inc.php
@@ -2,17 +2,22 @@
 <html lang="en">
     <head>
         <?php
-            if (filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL) == "localhost") {
-                echo '<base href="http://localhost:8888/sliceofpi06212017/public/">';
-            } else {
-                echo '<base href="https://www.pepster.com/">';
-            }
-        ?>
+        if (filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL) == "localhost") {
+            echo '<base href="http://localhost:8888/sliceofpi06212017/public/">';
+        } else {
+            echo '<base href="https://www.pepster.com/">';
+        }
         
+        $title = ['Index' => 'A Slice of Technology - Learning from Technology', 'About' => 'About John Pepp - Owner - Slice of Tecnology', 'Blog' => 'Online Blog Everyone', 'Calendar' => 'Calendar - Blog', 'Contact' => 'Contact - John Pepp', 'Login' => 'Login - Registration - a Slice of Technogy', 'Members_page' => 'Member Only Page', 'Daily' => "Daily Blog"];
+        ?>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Slice of Technology</title>
+        <meta name="description" content="A Slice of Technology - Learning from Technology" />
+        <meta name="keywords" content="tech, cms, tech news, calendar, social, technology, web development, web design, php, monthly calendar, blog, blog sites, social network, raspberry pi, social media, images photos, social sities" />
+        <title><?= $title[$pageName]; ?></title>
         <link rel="stylesheet" href="assets/css/stylesheet.css">
+        <link rel="shortcut icon" href="favicon.ico" >
         <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
 
@@ -26,19 +31,22 @@
                     <li><a href="index" <?php echo ($basename === 'index.php') ? 'class="current"' : NULL; ?>>Home</a></li>
                     <li><a href="about" <?php echo ($basename === 'about.php') ? 'class="current"' : NULL; ?>>About</a></li>                    
                     <li><a href="blog" <?php echo ($basename === 'blog.php') ? 'class="current"' : NULL; ?>>Blog</a></li>
+                    <li><a href="calendar" <?php echo ($basename === 'calendar.php') ? 'class="current"' : NULL; ?>>Calendar</a></li>
                     <li><a href="contact" <?php echo ($basename === 'contact.php') ? 'class="current"' : NULL; ?>>Contact</a></li>
-                    <?php 
-                    if (!is_logged_in() && $basename === 'blog.php') {
-                        if ($basename === 'login.php') {
-                            echo '<li><a href="login.php" class="current">Login</a></li>';
-                        } else {
-                            echo '<li><a href="login.php">Login</a></li>';
-                        }
-                    } elseif (is_logged_in()) {
-                        echo ($basename === 'members_page.php') ? '<li><a href="members_page.php" class="current">Members</a></li>' : '<li><a href="members_page.php">Members</a></li>';;
-                        echo '<li><a href="logout.php">Logout</a></li>';
+                    <?php
+                    if (!is_logged_in()) {
+                        echo ($basename === 'blog.php' || $basename === 'calendar.php') ? '<li><a href="login.php">Login</a></li>' : '<li><a class="notVisible" href="login.php">Login</a></li>';
                     }
-                    ?>                   
+                    ?>
+                    <?php if (is_logged_in()) { ?>
+                        <li><a href="members_page.php" <?php echo ($basename === 'members_page.php') ? 'class="current"' : NULL; ?>>Members</a></li>
+                    <?php } else { ?>
+                        <li><a class="notVisible" href="#">Members</a></li>
+                        <?php } ?>
+
+
+                    <?php echo ( is_logged_in() ) ? '<li><a href="logout.php">Logout</a></li>' : \NULL; ?>
+
                 </ul>
             </nav>
         </div>
