1c0d0b4702caeb17f20869ea8aaeeb25e411b1ef
John Pepp <jrpepp@pepster.com>
2017-09-19 09:00:24 -0400

    Add files via upload

--- Private/src/Read/Read.php
+++ Private/src/Read/Read.php
@@ -14,7 +14,7 @@ abstract class Read {
     public function getUserIds() {
         $db = DB::getInstance();
         $pdo = $db->getConnection();
-        $this->query = 'SELECT id, username, full_name, private FROM users';
+        $this->query = 'SELECT id, username, full_name, security_level, private FROM users';
         $this->stmt = $pdo->prepare($this->query);
         $this->stmt->execute();
         $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
@@ -43,6 +43,16 @@ abstract class Read {
 
         $this->stmt = $pdo->prepare($this->query); // Prepare the query:
         $this->stmt->execute([':page_name' => $page_name, ':column_pos' => $column_pos]); // Execute the query with the supplied user's parameter(s):
+        $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        return $this->data;
+    }
+
+    public function readBlog($page_name = "blog.php", $user_id = 65) {
+        $db = DB::getInstance();
+        $pdo = $db->getConnection();
+        $this->query = 'SELECT id, user_id, author, page_name, column_pos, image_path, heading, content, DATE_FORMAT(date_added, "%W, %M %e, %Y") as date_added, date_added as myDate FROM cms WHERE page_name=:page_name AND user_id=:user_id ORDER BY myDate DESC';
+        $this->stmt = $pdo->prepare($this->query); // Prepare the query:
+        $this->stmt->execute([':page_name' => $page_name, ':user_id' => $user_id]); // Execute the query with the supplied user's parameter(s):
         $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
         return $this->data;
     }
