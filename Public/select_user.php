193e09527ae861c2638cc206fa795b6e77f3209f
John Pepp <jrpepp@pepster.com>
2017-09-23 17:49:42 -0400

    Add files via upload

--- Public/select_user.php
+++ Public/select_user.php
@@ -19,24 +19,28 @@ $submit = filter_input(INPUT_POST, 'submit', FILTER_SANITIZE_FULL_SPECIAL_CHARS)
 
 if (isset($submit)) {
     $user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    if ((int)$user_id === 0) {
+    if ((int) $user_id === 0) {
         $user_id = getSysopId();
     }
     $data = $display->readBlog("blog.php", $user_id);
-    if ( (isset($_SESSION['user']) && $_SESSION['user']->id === (int)$user_id) || (isset($_SESSION['user']) && $_SESSION['user']->security_level === 'sysop') ) {
+    if ((isset($_SESSION['user']) && $_SESSION['user']->id === (int) $user_id) || (isset($_SESSION['user']) && $_SESSION['user']->security_level === 'sysop')) {
         $temp = true;
     } else {
         $temp = false;
     }
-    array_unshift($data, $temp);
-    
-    output($data);
+    if (!empty($data)) {
+        array_unshift($data, $temp);
+        output($data);
+    } else {
+        output(['status' => 'empty']);
+    }
 }
 /* End of your php routine(s) */
 
 /*
  * If you know you have a control error, for example an end-of-file the output it to the errorOutput() function
  */
+
 function errorOutput($output, $code = 500) {
     http_response_code($code);
     echo json_encode($output);
